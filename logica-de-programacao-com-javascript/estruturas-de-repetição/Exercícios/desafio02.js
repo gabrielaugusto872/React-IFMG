4ec93a083893c4058064d341ce77c98833210b00
Gabriel Augusto dos Santos <gabrielaugusto873@yahoo.com.br>
2025-10-02 15:27:42 -0300

    Update desafio02.js

--- logica-de-programacao-com-javascript/estruturas-de-repetição/Exercícios/desafio02.js
+++ logica-de-programacao-com-javascript/estruturas-de-repetição/Exercícios/desafio02.js
@@ -1,1 +1,21 @@
+let n;
+let soma;
+let n2;
+let n1;
 
+n = Number(prompt("Digite até onde a sequência deve ir"))
+
+for(i=0;i<n;i++){
+    if (i === 0){
+        console.log("0")
+        n2 = 0
+    } else if (i === 1){
+        console.log("1")
+        n1 = 1
+    } else {
+        soma = n2 + n1;
+        console.log(soma)
+        n2 = n1;
+        n1 = soma;
+    }
+}
