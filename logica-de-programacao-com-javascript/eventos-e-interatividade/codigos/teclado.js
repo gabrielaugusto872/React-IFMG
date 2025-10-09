7d64c7667b5e0358e047e8fa53366df33b443530
Gabriel Santos <gabrielaugusto873@yahoo.com.br>
2025-10-09 20:58:41 -0300

    att

--- logica-de-programacao-com-javascript/eventos-e-interatividade/codigos/teclado.js
+++ logica-de-programacao-com-javascript/eventos-e-interatividade/codigos/teclado.js
@@ -1,9 +1,9 @@
 const input = document.getElementById("meuInput");
 
-input.addEventListener("keydown", function(event){{
+input.addEventListener("keydown", function(event){
     console.log(`Tecla: ${event.key}, Código: ${event.code}`);
 
     if (event.key === "Enter"){
         console.log("Tecla Enter pressionada!")
     }
-}});
+});
